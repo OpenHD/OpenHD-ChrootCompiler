e77d797217ba2bc75c659e8425b562069f3fb58e
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-02-25 03:42:12 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -2,7 +2,9 @@ echo "hello world"
 cd /opt
 ls -a
 #./build.sh
-sudo apt install git
+sudo apt update
+wget -O - apt.radxa.com/bullseye-stable/public.key | sudo apt-key add - 
+sudo apt install -y git
 git clone https://github.com/OpenHD/OpenHD -recursive
 cd OpenHD
 bash  install_dep_ubuntu22.sh
