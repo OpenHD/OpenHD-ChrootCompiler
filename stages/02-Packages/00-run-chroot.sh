15484a457d73331ea0f83ea747b6309783a258d9
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 23:31:23 +0100

    speedup

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -7,17 +7,13 @@ apt update
  wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add - 
  sudo apt update
  sudo apt-mark hold linux-5.10-rock-5-latest
- sudo apt upgrade -y --allow-downgrades
+ #sudo apt upgrade -y --allow-downgrades
  echo "everything is now setup for compiling"
  fi
 echo "we've now entered a chroot environment, everything should be copied into /opt"
 cd /opt
-ls -a
 cd additionalFiles
-ls -a
 cd *
-ls -a
 bash build_chroot.sh
-ls -a
 echo "after building we can now push the contents outside the chroot"
 
