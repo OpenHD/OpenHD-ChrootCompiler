7cea044bfda131737dd303a6e17f59181ab66b8d
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 15:08:17 +0100

    add repo key radxa rock5

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash -e
 
+apt update
+ if [[ "${OS}" == "debian" ]]; then
+ #fix radxa's fuckup
+ export DISTRO=bullseye-stable
+ wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add - 
+ sudo apt update
+ sudo apt-mark hold linux-5.10-rock-5-latest
+ sudo apt upgrade
+ fi
 echo "we've now entered a chroot environment, everything should be copied into /opt"
 cd /opt
 ls -a
