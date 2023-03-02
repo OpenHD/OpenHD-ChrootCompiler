a3915c40efac8547165fdf751a57c2b9b6457144
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-02 14:22:09 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -3,6 +3,7 @@
 echo "we've now entered a chroot environment, everything should be copied into /opt"
 cd /opt
 ls -a
+cd additionalFiles
 bash build_chroot.sh
 echo "after building we can now push the contents outside the chroot"
 
