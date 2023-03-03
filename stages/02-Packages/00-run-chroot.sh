56541784dae25b18a30c9140b1d0f4f5e07aa480
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-03 16:06:38 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -8,7 +8,7 @@ apt update
  wget -O - apt.radxa.com/$DISTRO/public.key | sudo apt-key add - 
  sudo apt update
  sudo apt-mark hold linux-5.10-rock-5-latest
- #sudo apt upgrade -y --allow-downgrades
+ sudo apt upgrade -y --allow-downgrades
  echo "everything is now setup for compiling"
  fi
 echo "we've now entered a chroot environment, everything should be copied into /opt"
