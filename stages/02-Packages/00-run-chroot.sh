bfc1c7fe59514db98edc74096e656a7a57425dc2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-10-11 22:58:52 +0200

    chroot test

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -7,6 +7,15 @@ apt update
  sudo apt update
  echo "everything is now setup for compiling"
  fi
+ if [[ "${OS}" == "debian-X20" ]]; then
+    rm -Rf /etc/apt/sources.list.d/*
+    rm -Rf /etc/apt/sources.list
+    touch /etc/apt/sources.list
+    apt update
+    sudo sed -i 's/deb \[signed-by=\/usr\/share\/keyrings\/openhd-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/release\/deb\/debian bullseye main/deb \[signed-by=\/usr\/share\/keyrings\/openhd-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/dev-release\/deb\/debian sunxi main/' /etc/apt/sources.list.d/openhd-release.list
+    sudo sed -i 's/deb \[signed-by=\/usr\/share\/keyrings\/openhd-dev-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/dev-release\/deb\/debian bullseye main/deb \[signed-by=\/usr\/share\/keyrings\/openhd-dev-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/release\/deb\/debian sunxi main/' /etc/apt/sources.list.d/openhd-release.list
+ fi
+
 echo "we've now entered a chroot environment, everything should be copied into /opt"
 cd /opt
 cd additionalFiles
