16daf8d59b3324fe0ffb105a77af72f276846d36
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-13 01:29:17 +0100

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -18,6 +18,18 @@ apt remove -y dkms
     sudo sed -i 's/deb \[signed-by=\/usr\/share\/keyrings\/openhd-dev-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/dev-release\/deb\/debian bullseye main/deb \[signed-by=\/usr\/share\/keyrings\/openhd-dev-release-archive-keyring.gpg\] https:\/\/dl.cloudsmith.io\/public\/openhd\/release\/deb\/debian sunxi main/' /etc/apt/sources.list.d/openhd-release.list
  fi
 
+ if [[ "${OS}" == "radxa-debian-rock-cm3" ]]; then
+    PLATFORM_PACKAGES_HOLD="u-boot-radxa-zero3 radxa-system-config-common radxa-system-config-kernel-cmdline-ttyfiq0 radxa-firmware radxa-system-config-bullseye 8852be-dkms task-rockchip radxa-system-config-rockchip linux-image-radxa-cm3-rpi-cm4-io linux-headers-radxa-cm3-rpi-cm4-io linux-image-5.10.160-12-rk356x linux-headers-5.10.160-12-rk356x"
+     echo "Holding back platform-specific packages..."
+    for package in ${PLATFORM_PACKAGES_HOLD}; do
+        echo "Holding ${package}..."
+        apt-mark hold ${package} || true
+        if [ $? -ne 0 ]; then
+            echo "Failed to hold ${package}!"
+        fi
+    done
+ fi
+
 echo "we've now entered a chroot environment, everything should be copied into /opt"
 cd /opt
 cd additionalFiles
