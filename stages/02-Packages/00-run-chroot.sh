1ff0162bb7d825e72c209ff82b11795bf1fced40
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-14 18:44:46 +0200

    Update 00-run-chroot.sh

--- stages/02-Packages/00-run-chroot.sh
+++ stages/02-Packages/00-run-chroot.sh
@@ -45,4 +45,5 @@ cd additionalFiles
 bash build_chroot.sh
 echo "after building we can now push the contents outside the chroot"
 echo "___________________________________________________"
-
+echo ${OPENHD_VERSION} > /openhd_version2.txt
+echo ${OPENHD_VERSION} > /boot/openhd_version4.txt
